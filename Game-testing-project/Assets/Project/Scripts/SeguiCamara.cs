f041b1cb1e00c73aa988b3abac3812b5560abf0e
Moriarty-Rzer <64698169+Moriarty-Rzer@users.noreply.github.com>
2020-07-07 19:27:04 -0500

    Movimiento y Camara
    caminar, salto, seguimiento de camara

---
+++ Game-testing-project/Assets/Project/Scripts/SeguiCamara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguiCamara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject follow;
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		float posX = follow.transform.position.x;
+		float posY = follow.transform.position.y;
+
+		transform.position = new Vector3(posX, posY,transform.position.z);
+	}
+}
