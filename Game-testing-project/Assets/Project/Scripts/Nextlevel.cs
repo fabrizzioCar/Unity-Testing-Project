0eee11686b6fea4ce2cdcc5d642a97b4ac612725
Moriarty-Rzer <64698169+Moriarty-Rzer@users.noreply.github.com>
2020-07-12 20:28:21 -0500

    Pasar de level
    gaaaaa

---
+++ Game-testing-project/Assets/Project/Scripts/Nextlevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Nextlevel : MonoBehaviour
+{
+    public GameObject follow;
+
+    public string levelName;
+    private bool Puerta = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Puerta= true ;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Puerta = true ;
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Puerta && Input.GetKey("e") && follow.transform.childCount == 0 )
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
